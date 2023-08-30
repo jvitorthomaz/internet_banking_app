e42ecbcacc764e3a0b74954140e06a43ba88d131
vitorthomaz <tfs01@evosystems.com.br>
2023-08-29 23:14:06 -0300

    Feature: Header Layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:internet_banking_app/components/sections/header.dart';
 
 class Home extends StatelessWidget {
 const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Header(),
+        ],
+      ),
+    );
   }
 }
