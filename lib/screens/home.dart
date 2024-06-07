b1a26b769e9de1ca12f69a01b9d0f2f2fe051e1f
jvitorthomaz <jovitorthomaz2@gmail.com>
2024-06-07 20:07:16 -0300

    feat: first screen card implementation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:internet_banking_app/components/box_card.dart';
 import 'package:internet_banking_app/components/color_dot.dart';
 import 'package:internet_banking_app/components/sections/header.dart';
+import 'package:internet_banking_app/components/sections/recent_activity.dart';
 import 'package:internet_banking_app/themes/theme_colors.dart';
 
 class Home extends StatelessWidget {
@@ -13,9 +14,7 @@ const Home({ Key? key }) : super(key: key);
       body: Column(
         children: <Widget>[
           Header(),
-          BoxCard(
-            boxContent: ColorDot(color: ThemeColors.recentActivity['spent'])
-          ),
+          RecentActivity()
         ],
       ),
     );
