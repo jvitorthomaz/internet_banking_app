f2b3d7ad2ae8668584411cea7423d02f48f71370
jvitorthomaz <jovitorthomaz2@gmail.com>
2024-06-08 02:01:09 -0300

    feat: account points section implementation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:internet_banking_app/components/box_card.dart';
 import 'package:internet_banking_app/components/color_dot.dart';
 import 'package:internet_banking_app/components/sections/account_actions.dart';
+import 'package:internet_banking_app/components/sections/account_points.dart';
 import 'package:internet_banking_app/components/sections/header.dart';
 import 'package:internet_banking_app/components/sections/recent_activity.dart';
 import 'package:internet_banking_app/themes/theme_colors.dart';
@@ -11,12 +12,19 @@ const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Column(
-        children: <Widget>[
-          Header(),
-          RecentActivity(),
-          AccountActions(),
+    return  Scaffold(
+      body: ListView(
+        children: const [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Header(),
+            RecentActivity(),
+            AccountActions(),
+            AccountPoints(),
+      
+          ],
+        ),
         ],
       ),
     );
